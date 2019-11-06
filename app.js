2d9b92d7d4b07706c7d7bf2ebe4edc319cdd116f
Matto <matt.mcquain@gmail.com>
2019-11-05 21:06:20 -0800

    working on closures

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-var count = 0;
+var birds = 3;
 
-function countBirds() {
-  count += 1;
-  return count + ' birds';
+function dogHouse() {
+  var dogs = 8;
+  function showDogs() {
+    console.log(dogs);
+  }
+  return showDogs;
 }
 
-function countDogs() {
-  count += 1;
-  return count + ' dogs';
-}
+var getDogs = dogHouse();
+
+getDogs(); // 8
+
+// The dogs variable is declared in the outer function, and the inner function has access to it. If the inner function is returned and assigned to a variable, the private scope of the outer function remains. 
+
+// We can call the returned inner function and get access to or even modify any private variables. 
