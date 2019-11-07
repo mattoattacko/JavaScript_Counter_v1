4c9ff3dabb213a6087069d54b0e163df3d3fae6d
Matto <matt.mcquain@gmail.com>
2019-11-06 19:44:03 -0800

    working on counter function

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-var birds = 3;
+var count = 0;
 
-function dogHouse() {
-  var dogs = 8;
-  function showDogs() {
-    console.log(dogs);
+function makeBirdCounter() {
+  var count = 0;
+  return function() {
+    count += 1;
+    return count + ' birds';
   }
-  return showDogs;
 }
-
-var getDogs = dogHouse();
-
-getDogs(); // 8
-
-// The dogs variable is declared in the outer function, and the inner function has access to it. If the inner function is returned and assigned to a variable, the private scope of the outer function remains. 
-
-// We can call the returned inner function and get access to or even modify any private variables. 
